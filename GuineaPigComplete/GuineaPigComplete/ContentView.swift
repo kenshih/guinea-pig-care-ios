40b33095922de3d46eed7f115df26207155dc427
Ken Shih <ken.shih@gmail.com>
2024-03-27 12:15:12 -0700

    feature(init): create ios project skeleton

---
+++ GuineaPigComplete/GuineaPigComplete/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuineaPigComplete
+//
+//  Created by Ken on 3/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Guinea Pig Complete!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
